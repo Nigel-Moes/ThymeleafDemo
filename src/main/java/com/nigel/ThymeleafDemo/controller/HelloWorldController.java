9cdd8e820f2fafa7f929e1a7648c2a249f10aba6
Nigel <mail@nigelmoes.nl>
2024-03-25 13:53:14 -0500

    Added data to Spring MVC model

--- src/main/java/com/nigel/ThymeleafDemo/controller/HelloWorldController.java
+++ src/main/java/com/nigel/ThymeleafDemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.nigel.ThymeleafDemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,25 @@ public class HelloWorldController {
     // controller method to process the form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String showCaps(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hello " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
